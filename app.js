15cdaee236e66597a7b7aff854cc1ebb23ca6a7a
S0ngyoungjun <clesaaaaaa@gmail.com>
2023-12-22 18:05:38 +0900

    1

---
+++ app.js
@@ -0,0 +1,17 @@
+class ForExam {
+  constructor() {
+    this.initData();
+  }
+  initData() {
+
+    const config = {
+      baseArray: ['피카츄', '라이츄', '꼬부기'],
+      specialProblems: [10, 16, 17, 18, 19, 20],
+      htmlTagProblems: [22, 23, 25, 27 , 29, 30],
+      startProblemIndex: 1,
+      endProblemIndex: 30
+    };
+
+    for (let i = config.startProblemIndex; i <= )
+  }
+}
